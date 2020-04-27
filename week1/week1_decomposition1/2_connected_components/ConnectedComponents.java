9be7c432a6c23ac4c6efd26d6f56bf729a438ab4
Sarthak Srivastava <sarth1597@gmail.com>
2020-04-28 01:18:23 +0530

    Add files via upload

--- week1/week1_decomposition1/2_connected_components/ConnectedComponents.java
+++ week1/week1_decomposition1/2_connected_components/ConnectedComponents.java
@@ -1,11 +1,30 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class ConnectedComponents {
     private static int numberOfComponents(ArrayList<Integer>[] adj) {
         int result = 0;
         //write your code here
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i = 0; i<adj.length; i++)
+        {
+            if(!hs.contains(i))
+            {   result++;
+                // hs.add(i);
+                dfs(adj,i,hs);
+            }
+        }
         return result;
+    }
+    private static void dfs(ArrayList<Integer>[] adj, int src, HashSet<Integer> visited)
+    {
+        if(visited.contains(src))
+            return;
+        visited.add(src);
+        for(int i: adj[src])
+        {
+            if(!visited.contains(i))
+            dfs(adj, i, visited);
+        }
     }
 
     public static void main(String[] args) {
